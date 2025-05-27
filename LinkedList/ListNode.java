ae81b4a9ba75512a5b313600e0c2e7afddb2cd3d
Anurag-Deshmukh25 <anurag.deshmukh25@gmail.com>
2025-05-28 00:44:58 +0530

    Initial Commit

--- LinkedList/ListNode.java
+++
@@ -1,9 +0,0 @@
-package LinkedList;
-
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(){}
-    ListNode(int x) { val = x; }
-    ListNode(int x, ListNode next) { val = x; next = next; }
-}
