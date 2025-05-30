b19cb35404f618bdf1d8b084fd6d95298a0078ef
Anurag-Deshmukh25 <anurag.deshmukh25@gmail.com>
2025-05-30 14:59:13 +0530

    26. Remove Duplicates from sorted array

---
+++ Arrays/RemDupSortedArr.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class RemDupSortedArr{
+    public int removeDuplicated(int[] nums){
+        int i = 0;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != nums[i]){
+                nums[++i] = nums[j];
+            }
+        }
+
+        return i+1;
+    }
+    public static void main(String[] args) {
+        RemDupSortedArr remover = new RemDupSortedArr();
+        int[] nums = {1, 1, 2, 2, 3, 4, 4};
+        int newLength = remover.removeDuplicated(nums);
+
+        // Print the modified array
+        for(int k = 0; k < newLength; k++){
+            System.out.print(nums[k] + " ");
+        }
+    }
+}
