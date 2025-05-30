9a297486fa407cfa64d3bbf6957df382356ac2f6
Anurag-Deshmukh25 <anurag.deshmukh25@gmail.com>
2025-05-30 17:54:51 +0530

    141. Linked List Cycle

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head){
+        if(head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        LinkedListCycle cycleChecker = new LinkedListCycle();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = head; // Creating a cycle
+
+        boolean hasCycle = cycleChecker.hasCycle(head);
+        System.out.println("Has Cycle: " + hasCycle); // Output: true
+    }
+}
