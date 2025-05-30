3c6daf51c2b741dda89ae8efb60ae0d3a9c83071
Anurag-Deshmukh25 <anurag.deshmukh25@gmail.com>
2025-05-30 18:06:57 +0530

    35. Search Insert Position

---
+++ BinarySearch/SearchInsertPos.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class SearchInsertPos {
+    public int searchInsertPos(int[] nums, int target){
+        int left = 0, right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+    public static void main(String[] args) {
+        SearchInsertPos searcher = new SearchInsertPos();
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        int position = searcher.searchInsertPos(nums, target);
+        
+        System.out.println("Insert Position: " + position); // Output: 2
+    }
+}
