62fcbed6b96bca7c7bd1be52d97295680b9d4841
Anurag-Deshmukh25 <anurag.deshmukh25@gmail.com>
2025-05-31 19:09:23 +0530

    136. Single Number

---
+++ BitManipulation/SingleNum.java
@@ -0,0 +1,17 @@
+package BitManipulation;
+
+public class SingleNum {
+    public int singleNumber(int[] nums){
+        int result = nums[0];
+        for(int i = 1; i < nums.length; i++) result ^= nums[i];
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SingleNum singleNumFinder = new SingleNum();
+        int[] nums = {4, 1, 2, 1, 2};
+        int singleNumber = singleNumFinder.singleNumber(nums);
+        System.out.println("The single number is: " + singleNumber);
+    }
+}
